e414110598deffd249db4217ee50598ba3509867
Pushpa <pushpasri12@gmail.com>
2019-09-23 14:45:09 +0530

    task manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { ViewalltaskComponent } from './viewalltask/viewalltask.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtaskComponent,
+    ViewalltaskComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'addtask',component:AddtaskComponent},
+    { path:'viewalltask',component:ViewalltaskComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
